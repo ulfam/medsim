2739ce829bf7bbc68c89da4afab63735580a9bb3
Yana Ulyanenko <yanaulyanenko@Yanas-MacBook-Pro.local>
2023-12-18 13:43:09 +0000

    cleaned comments

--- knn.py
+++ knn.py
@@ -8,12 +8,10 @@ def search_similar_medicines(medicine_name, k=20):
     print('start read data...')
     if medicine_name.lower()[0] in string.ascii_lowercase:
         X_tfidf = joblib.load('theindependentpharmacy_tfidf.joblib')
-        # nn = joblib.load('knn_model_theindependentpharmacy.joblib')
         new_df = pd.read_csv('theindependentpharmacy_names_links.csv')
         index = faiss.read_index("flat_eng.index")
     else:
         X_tfidf = joblib.load('vseapteki_tfidf.joblib')
-        # nn = joblib.load('knn_model_vseapteki.joblib')
         new_df = pd.read_csv('vseapteki_names_links.csv')
         index = faiss.read_index("flat_rus.index")
     vectors = X_tfidf.astype('float32').toarray()
@@ -22,7 +20,7 @@ def search_similar_medicines(medicine_name, k=20):
     print(f'Name of medicine is: {medicine_name_processed}')
     i = list(new_df['Name_short'].values).index(medicine_name_processed)
     print(f'Index of target value is: {i}')
-    D, I = index.search(vectors[[i]], k)  # Возвращает результат: Distances, Indices
+    D, I = index.search(vectors[[i]], k)
     ans_prev = new_df.loc[I[0][1:]][['name', 'link', 'Name_short']]
     ans = ans_prev[ans_prev['Name_short']!=medicine_name_processed][['name', 'link']].values[:10]
     return ans
